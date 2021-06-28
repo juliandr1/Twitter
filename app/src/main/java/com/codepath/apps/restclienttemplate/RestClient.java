d7ce29320c51799f2b4dbf7ebf9a18b6f9541a1c
juliandr1 <juli57vivi@outlook.com>
2021-06-28 16:22:51 -0400

    User can sign in to Twitter using OAuth login

--- app/src/main/java/com/codepath/apps/restclienttemplate/RestClient.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/RestClient.java
@@ -6,6 +6,7 @@ import com.codepath.asynchttpclient.RequestParams;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.codepath.oauth.OAuthBaseClient;
 import com.github.scribejava.apis.FlickrApi;
+import com.github.scribejava.apis.TwitterApi;
 import com.github.scribejava.core.builder.api.BaseApi;
 
 /*
@@ -21,9 +22,9 @@ import com.github.scribejava.core.builder.api.BaseApi;
  * 
  */
 public class RestClient extends OAuthBaseClient {
-	public static final BaseApi REST_API_INSTANCE = FlickrApi.instance(FlickrApi.FlickrPerm.WRITE); // Change this
-	public static final String REST_URL = "https://api.flickr.com/services"; // Change this, base API URL
-	public static final String REST_CONSUMER_KEY = BuildConfig.CONSUMER_KEY;       // Change this inside apikey.properties
+	public static final BaseApi REST_API_INSTANCE = TwitterApi.instance();;
+	public static final String REST_URL = "https://api.twitter.com/1.1";
+	public static final String REST_CONSUMER_KEY = BuildConfig.CONSUMER_KEY; // Change this inside apikey.properties
 	public static final String REST_CONSUMER_SECRET = BuildConfig.CONSUMER_SECRET; // Change this inside apikey.properties
 
 	// Landing page to indicate the OAuth flow worked in case Chrome for Android 25+ blocks navigation back to the app.
@@ -40,9 +41,6 @@ public class RestClient extends OAuthBaseClient {
 				null,  // OAuth2 scope, null for OAuth1
 				String.format(REST_CALLBACK_URL_TEMPLATE, context.getString(R.string.intent_host),
 						context.getString(R.string.intent_scheme), context.getPackageName(), FALLBACK_URL));
-
-		REST_API_INSTANCE = TwitterApi.instance();
-		REST_URL = "https://api.twitter.com/1.1";
 	}
 	// CHANGE THIS
 	// DEFINE METHODS for different API endpoints here
